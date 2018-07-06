745121bbd8d02a78ba3e3581ae8ec464ab4d0025
andywindowsmac <adilkhankenzhetaev@gmail.com>
2018-07-06 21:50:55 +0600

    feat(Head): restrict min height

--- app/components/Head.tsx
+++ app/components/Head.tsx
@@ -11,6 +11,17 @@ const Head: SFC<IHeadOwnProps> = ({ title }): JSX.Element => (
     <title>{title}</title>
     <meta charSet='utf-8' />
     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+    <link
+      rel='stylesheet'
+      href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css'
+      integrity='sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy'
+      crossOrigin='anonymous'
+    />
+    <style>{`
+      body {
+        min-height: 100vh;
+      }
+    `}</style>
   </NextHead>
 );
 
