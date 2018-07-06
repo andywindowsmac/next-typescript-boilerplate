31af04493fdf32ecbcb5e7ce80d2e1ff9c26ffca
andywindowsmac <adilkhankenzhetaev@gmail.com>
2018-07-06 21:51:21 +0600

    feat(index): extracct Loader

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,3 +1,4 @@
 export { default as DataFetch } from './DataFetch';
 export { default as Head } from './Head';
 export { default as Layout } from './Layout';
+export { default as Loader } from './Loader';
