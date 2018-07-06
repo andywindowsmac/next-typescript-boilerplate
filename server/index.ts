aa6219173848d1df49fb279aad34e3de149b42de
andywindowsmac <adilkhankenzhetaev@gmail.com>
2018-07-07 01:21:18 +0600

    feat: cache homepage

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,22 @@
+
 import * as express from 'express';
 import * as next from 'next'
+
+import Cache from './Cache';
 
 const PORT = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dir: 'app', dev });
 const handle = app.getRequestHandler();
+const renderAndCache = Cache(app);
 
 (async () => {
   await app.prepare();
   const server = express();
+
+  server.get('/', (req, res) => {
+    renderAndCache(req, res, '/', null);
+  })
 
   server.get('*', (req, res) => handle(req, res));
 
